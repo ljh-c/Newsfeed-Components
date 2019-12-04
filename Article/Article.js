0512ce6e811cd4c7b0d2f217d6fca539c4982730
ljh-c <57099181+ljh-c@users.noreply.github.com>
2019-12-03 22:18:28 -0800

    articleMaker function returns article component

--- Article/Article.js
+++ Article/Article.js
@@ -101,9 +101,40 @@ const data = [
 
   Hint: You will need to use createElement more than once here!
 
-  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
+  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above. */
 
-  Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
+function articleMaker(articleData) {
+  const article = document.createElement('div');
+  article.classList.add('article');
+
+  const title = document.createElement('h2');
+  title.textContent = articleData['title'];
+  article.appendChild(title);
+
+  const date = document.createElement('p');
+  date.classList.add('date');
+  date.textContent = articleData['date'];
+  article.appendChild(date);
+
+  const paragraph1 = document.createElement('p');
+  paragraph1.textContent = articleData['firstParagraph'];
+  article.appendChild(paragraph1);
+
+  const paragraph2 = document.createElement('p');
+  paragraph2.textContent = articleData['secondParagraph'];
+  article.appendChild(paragraph2);
+
+  const paragraph3 = document.createElement('p');
+  paragraph3.textContent = articleData['thirdParagraph'];
+  article.appendChild(paragraph3);
+
+  const expand = document.createElement('span');
+  expand.classList.add('expandButton');
+
+  return article;
+}
+
+  /* Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
 
   Step 3: return the entire component.
 
@@ -112,3 +143,17 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+// function buttonCreator(buttonText){
+//   const button = document.createElement('button');
+
+//   button.textContent = buttonText;
+
+//   button.classList.add('button');
+
+//   button.addEventListener('click', (e) => {
+//       console.log('clicked!');
+//   });
+
+//   return button;
+// }
