27acc2e8e67edf75cbb02e86370617e9a04cab28
ljh-c <57099181+ljh-c@users.noreply.github.com>
2019-12-03 22:23:57 -0800

    adds expand article functionality

--- Article/Article.js
+++ Article/Article.js
@@ -101,7 +101,11 @@ const data = [
 
   Hint: You will need to use createElement more than once here!
 
-  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above. */
+  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above. 
+
+  /* Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
+
+  Step 3: return the entire component. */
 
 function articleMaker(articleData) {
   const article = document.createElement('div');
@@ -130,15 +134,14 @@ function articleMaker(articleData) {
 
   const expand = document.createElement('span');
   expand.classList.add('expandButton');
+  expand.addEventListener('click', () => {
+    article.classList.toggle('article-open');
+  });
 
   return article;
 }
 
-  /* Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
-
-  Step 3: return the entire component.
-
-  Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
+  /* Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
 
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
